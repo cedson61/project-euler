5ba87c1637ae3b2e15c89b06ca61ba0be8a25e8d
cedson61 <chase.edson@gmail.com>
2018-11-21 12:30:05 -0800

    init

---
+++ pr007.py
@@ -0,0 +1,31 @@
+import time
+ 
+
+def nth_prime(n):
+	prime = 2
+	by = 1
+	iter = 3
+	while by < n:
+		if isprime(iter):
+			prime = iter
+			by += 1
+		iter += 2
+	return prime
+
+def isprime(n):
+	while True:
+		b = True
+		if n % 2 ==0:
+		 b = False
+		d = 3
+		while d < n**0.5+1:
+			if n % d == 0: b = False
+			d += 2
+		print d
+		return b
+ 
+start = time.time()
+butthole = nth_prime(10001)
+elapsed = time.time() - start
+
+print ("%s found in %s seconds. eat pant euler.") % (butthole,elapsed)
